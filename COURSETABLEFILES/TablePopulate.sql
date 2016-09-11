eb44a41c03e3c944e84e47b0df1077c94f829a9b
cgrealis <grealish.c@gmail.com>
2016-09-11 16:38:28 -0500

    Adding TablePopulate

---
+++ COURSETABLEFILES/TablePopulate.sql
@@ -0,0 +1,79 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1265\cocoasubrtf210
+{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;}
+\margl1440\margr1440\vieww10800\viewh9000\viewkind0
+\deftab720
+\pard\pardeftab720
+
+\f0\fs22 \cf0 \'97-Insert statements for person table\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (1, Margarito Button, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (2, Kathi Carey, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (3, Waylon Russo, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (4, Carlo Coley, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (5, Michale Stallworth, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (6, Jaleesa Hammonds, 0, Department);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (7, Herschel Upton, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (8, Vennie Aguilera, 2, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (9, Barbie Barron, 4, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (10, Dione Mcclung, 2, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (11, Celine Causey, 3, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (12, Maybelle Wolford, 1, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (13, Ashly Chung, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (14, Dot Waldron, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (15, Richie Shuman, 4, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (16, Karan Benjamin, 3, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (17, Alissa Corbett, 2, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (18, Malinda Nickerson, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (19, Yetta Dyson, 1, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (20, Willia Crisp, 4, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (21, Awilda Paredes, 3, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (22, Michal Eng, 4, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (23, Allyn Pfeiffer, 2, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (24, Alethia Trent, 1, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (25, Virgina Tavares, 4, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (26, Nikia Vest, 2, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (27, Tillie Hamblin, 4, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (28, Lamont Fraley, 2, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (29, Lincoln Pounds, 4, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (30, Kimbery Parson, 3, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (31, Quyen Lovejoy, 1, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (32, Macie Culbertson, 4, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (33, Kendrick Hummel, 2, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (34, Halina Passmore, 2, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (35, Marquitta Dowd, 1, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (36, Mayola Nowlin, 4, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (37, Buddy Strong, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (38, Roxane Hartmann, 3, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (39, Adell Delossantos, 1, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (40, Laurel Samples, 1, MATH);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (41, Clelia Dorman, 3, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (42, Kasha Burks, 4, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (43, Kera Bethea, 3, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (44, Kena Shoemaker, 2, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (45, Mel Jaramillo, 1, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (46, Kassandra Maupin, 1, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (47, Isiah Sturgeon, 1, CS);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (48, Reita Holguin, 4, HIST);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (49, Blossom Sorrell, 4, THEO);\
+INSERT INTO person (Person_number, Name, Class, Major) VALUES (50, Maida Sams, 1, MATH);\
+\
+\
+\
+\'97-Insert statements for course table\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Intro to Computer Science, CS, 3770, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Data Structures, CS, 7680, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Database, CS, 8620, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Christ and the Moral Life, THEO, 6640, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Teachings of the Catholic Church, THEO, 5690, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Theology of the Body, THEO, 4020, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (World History I, HIST, 5060, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (World History II, , HIST, 7940, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (American History I, HIST, 2320, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (American History II, HIST, 4780, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Space History, HIST, 3100, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Linear Algebra, MATH, 1280, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Calculus I, MATH, 9840, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Calculus II, MATH, 5870, 4);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Calculus III, MATH, 8770, 3);\
+INSERT INTO course (Course_name, Department, Course_number, Credit_hours) VALUES (Discrete Mathematics, MATH, 9530, 4);\
+}
